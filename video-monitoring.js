aaa64215648f20078fe96675a2d0e749b231f383
William Chen <wichen@microsoft.com>
2012-10-16 17:02:01 -0700

    Added support to monitor specific format codes and bug fixes
    - Support to check for specific video format codes (103, 1002, etc)
    - Pushed errors until the end
    - Changed the script workflow to integrate HEAD request and manifest
    matching together
    - Bugfixes

--- video-monitoring.js
+++ video-monitoring.js
@@ -3,78 +3,131 @@ Scripter.Logging = 1;
 // ====
 // Variables
 var contentIdRegexp = '\/view\\?entitytype=video\&contentId=([^"&]*)';
-var manifestBaseUrl = "http://edge3.catalog.video.msn.com/videobyuuid.aspx?uuid=";
+var manifestBaseUrl = "http://edge%ENDPOINT%.catalog.video.msn.com/videobyuuid.aspx?uuid=";
+var numManifestServers = 5;
+var formatCode = ['1002', '103'];
 var filetype = 'mp4';
+
+var errors = [];
 
 
 // ====
 // Functions
-var checkResponse = function(url) {
-  if(typeof url == 'string') {
-    Scripter.Log ("Fetching url: " + url);
+
+/**
+ * TODO description
+ **/
+var fetchContent = function(url, request) {
+  request = typeof request == 'undefined'? 'get' : request;
+
+  if(typeof url != 'string') {
+    // TODO programming error
+  }
+  if(typeof request != 'string' && /(get|head)/i.test(request)) {
+    // TODO programming error
+  }
+
+  if(/head/i.test(request)) {
+    Scripter.Log("Issuing HEAD request for: " + url);
+    if(!KNWeb.Head(url)) {
+      Scripter.SetError(-90400, true);
+      KNWeb.SetErrorDetails(-90400,"Cannot get header for: " + url,"","","");
+    }
+  }
+  else {
+    Scripter.Log ("Issuing GET request for: " + url);
     if(!KNWeb.Get(url)) {
       Scripter.SetError(-90404, true);
       KNWeb.SetErrorDetails(-90404, "** ERROR: Failed to get content for url: " + url);
     }
   }
-  //Scripter.Log("Checking response header for " + KNWeb.GetURL(0));
+
   header = KNWeb.GetResponseHeaders(0);
   Scripter.Log("Received a response of '" + header.match(/[^\r\n]*/) + "'.");
+  /*
   if(!/200/.test(header)) {
     Scripter.SetError(-99102, true);
     KNWeb.SetErrorDetails(-99102,"Did not respond with code 200" + contentId,"","","");
   }
+  */
 
+  if(/get/i.test(request) && /200/.test(header)) {
+    return KNWeb.GetContent(0);
+  }
+  return /200/.test(header);
 }
 
-// NOTE: untested and unused until December 2012 (?)
-var checkVideoResponse = function(url) {
-  checkResponse(url);
-  var header = KNWeb.GetResponseHeaders(0);
-  var regexs = new Array(/Content-Length: (\d+)/, /Content-Type: ([^\r\n ;]*)/);
+/**
+ * Get a random endpoint from 1 to max, inclusive
+ * @param baseUrl (string): url with %ENDPOINT% to hit a random MSN server
+ * @return: random url
+ **/
+// TODO better description
+var fetchManifestUrl = function(baseUrl) { 
+  serverEndpoint = Math.floor((Math.random()*numManifestServers)+1);
+  return manifestBaseUrl.replace("%ENDPOINT%", serverEndpoint);
+}
 
-  // check for content-length and content-type
-  for(i in regexs) {
-    var regex = regexs[i];
+/**
+ * Fetch video urls specified by format code.
+ * @param manifestUrl (string): url to MSN manifest
+ * @param format (undefined, string, array): format code to the video to check
+ * If format === undefined, all video urls in manifest will be returned
+ * @return: an array of video urls
+ * TODO update description
+ **/
+var verifyManifest = function(manifestUrl, format) {
+  format = typeof format === 'undefined'? ['*'] : format;
+  format = typeof format === 'string'? [format] : format;
+  if(! format instanceof Array) {
+    // TODO programming error
+  }
+  content = fetchContent(manifestUrl);
 
-    if(!regex.test(header)) {
-      Scripter.SetError(-90400, true);
-      KNWeb.SetErrorDetails(-90400, "Header information not found in the header", "", "", "");
-    }
-    response = header.match(regex)[1];
-    if(response.length < 1) {
-      Scripter.SetError(-90411, true);
-      KNWeb.SetErrorDetails(-90411, "Server did not reply information to the header request", "", "", "");
-    }
-
-    // Checking content-length is more than 1000k
-    if(i==0 && parseInt(response) < 1000) {
-      Scripter.SetError(-90411, true);
-      KNWeb.SetErrorDetails(-90411, "Content size is not big enough to be a video. (Reported "+response+")", "", "", "");
-    }
-
-    // Checking content-type
-    if(i==1 && ! /video/.test(response)) {
-      Scripter.SetError(-90415, true);
-      KNWeb.SetErrorDetails(-90415, "Content type is not a video. (Reported "+response+")", "", "", "");
-    }
+  if(typeof content == 'boolean') {
+    // TODO error - trouble downloading manifest
   }
 
+  Scripter.Log("Processing format(s): " + format);
+  for(i=0; i<format.length; i++) {
+    regex = new RegExp('<videoFile formatCode="' + format[i] + '".*?<\/videoFile>', 'gim');
+    videoFiles = content.match(regex);
+
+    if(!videoFiles) {
+      Scripter.Log("** FAILED on format " + format[i] + ". Logging error");
+      errors.push("Manifest " + manifestUrl + " does not have video content for format code " + format[i]);
+      continue;
+    }
+
+    for(j=0; j<videoFiles.length; j++) {
+      regex = new RegExp('http[^"]*\.' + filetype, 'gi');
+      videoUris = videoFiles[j].match(regex);
+
+      if(!videoUris) {
+        Scripter.Log("** FAILED on format " + format[i] + ". Logging error");
+        errors.push("Manifest " + manifestUrl + " does not have video content for format code " + format[i]);
+        continue;
+      }
+
+      for(k=0; k<videoUris.length; k++) {
+        Scripter.Log("Found video url for format " + format[i] + ": " + videoUris[k]);
+        fetchContent(videoUris[k], 'head');
+      }
+    }
+  }
 }
 
 // ====
 // Script
-var content = KNWeb.GetContent(0);
-checkResponse();
+var content = fetchContent(KNWeb.GetURL(0));
 
 Scripter.Log("Response content length: " + content.length);
 var contentIds = new Array();
-var videoUrls = new Array();
 var match = content.match(new RegExp(contentIdRegexp, 'gi'));
 
 if(!match) {
   Scripter.SetError(-99503, true);
-  KNWeb.SetErrorDetails(-99503, "No matching video ids found!");
+  KNWeb.SetErrorDetails(-99503, "No video ids found in the supplied pano definition!");
 }
 
 Scripter.Log("\nFound " + match.length + " video ids");
@@ -87,35 +140,24 @@ for(i=0; i<match.length; i++) {
   contentIds.push(contentId);
 }
 
-// Fetching manifests
+// Processing manifests
 regex = new RegExp('http[^"]*\.'+filetype, 'gi');
 Scripter.Log("\nProcessing " + contentIds.length + " manifests");
 for(j in contentIds) {
   contentId = contentIds[j];
   Scripter.Log("\nFetching manifest for: " + contentId);
 
-  checkResponse(manifestBaseUrl + contentId);
-  matchedUrls = KNWeb.GetContent(0).match(regex);
-  for(i=0; i<matchedUrls.length; i++) {
-    Scripter.Log("Found video url: " + matchedUrls[i]);
-    videoUrls.push(matchedUrls[i]);
-  }
+  manifestUrl = fetchManifestUrl(manifestBaseUrl) + contentId;
+  response = verifyManifest(manifestUrl, formatCode);
 }
 
-if(videoUrls.length < 1) {
-  Scripter.SetError(-99215, true);
-  KNWeb.SetErrorDetails(-99215, "No mp4 urls found in " + KNWeb.GetUrl(0));
-}
-
-// Fetching HEAD for videos
-// NOTE Once HEAD requests are supported by MSN, the test should pass
-Scripter.Log("\nProcessing " + videoUrls.length + " videos");
-for (j in videoUrls) {
-  url = videoUrls[j];
-  Scripter.Log("\nRequesting HEAD information for: " + url);
-  if(!KNWeb.Head(url)) {
-    Scripter.SetError(-90400, true);
-    KNWeb.SetErrorDetails(-90400,"Cannot get header for: " + url,"","","");
+// Display all the errors
+// TODO find an appropriate error number
+if(errors.length > 0) {
+  Scripter.Log("\n\n========\nError Log\n========\n");
+  for(i in errors) {
+    Scripter.Log(+i+1 + ") " + errors[i]);
   }
-  checkResponse();
+  Scripter.SetError(-99503, true);
+  KNWeb.SetErrorDetails(-99503, "No video ids found in the supplied pano definition!");
 }
